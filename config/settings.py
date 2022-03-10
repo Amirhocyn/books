c0006af98ada5346898b97b576fe65c038cc2e24
a.noroozi <42445432+Amirhocyn@users.noreply.github.com>
2022-03-10 11:52:13 +0330

    create book model and change the URL structure to UUID and test our model

--- config/settings.py
+++ config/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     # Local
     'accounts',
     'pages',
+    'books',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- config/urls.py
+++ config/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
 
     # Local urls,
     path('', include('pages.urls')),
+    path('books/', include('books.urls'))
 ]
